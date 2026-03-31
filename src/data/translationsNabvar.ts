0c9937131e7891a2949c688674dac81cb9e159cf
Jeferson-Ruiz <jefersonruiz578@gmail.com>
2026-03-31 08:18:19 -0500

    feat(data): añadir archivo con traducciones para navegación y contenido

---
+++ src/data/translationsNabvar.ts
@@ -0,0 +1,41 @@
+export const defaultLang = "es";
+
+export const translations = {
+  es: {
+    nav: {
+      inicio: "Inicio",
+      quienesSomos: "Quienes Somos",
+      trayectoria: "Trayectoria",
+      instituto: "Instituto de Investigación",
+      servicios: "Servicios",
+      contactenos: "Contáctenos",
+    },
+    social: {
+      facebook: "Seleccione para ir a la pagina de Facebook",
+      instagram: "Seleccione para ir a la pagina de Instagram",
+      tiktok: "Seleccione para ir a la pagina de Tiktok",
+    },
+    languages:{
+      selectPrompt: "Seleccione para ver las opciones de navegación",
+    }
+  },
+  
+  en: {
+    nav: {
+      inicio: "Home",
+      quienesSomos: "About Us",
+      trayectoria: "History",
+      instituto:"Research Institute",
+      servicios: "Services",
+      contactenos: "Contact Us",
+    },
+    social: {
+      facebook: "Select to go Facebook page",
+      instagram: "Select to go Instagram page",
+      tiktok: "Select to go Tiktok page",
+    },
+    languages:{
+      selectPrompt: "Select to see navigation options",
+    }
+  },
+};
