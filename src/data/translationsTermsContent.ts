0628fcc1151989e22e9df1b6112566f3319c4b80
Jeferson-Ruiz <jefersonruiz578@gmail.com>
2026-04-27 11:54:36 -0500

    feat(translations): crear archivo de traducciones para políticas de privacidad

---
+++ src/data/translationsTermsContent.ts
@@ -0,0 +1,101 @@
+export const defaultLang = "es";
+
+export const translationsTermsContent = {
+  es:{
+  title: "Términos y condiciones",
+  text: `
+  Los presentes Términos y Condiciones se publican para así suministrar a la información pertinente para todos aquellos aspectos en relación al uso adecuado y dinámico de nuestros canales digitales, también para garantizar el adecuado tratamiento de datos.
+
+  Nordvital Ips es una empresa que sostiene como objeto social la prestación de servicios de salud, todo nuestro esfuerzo y el constante crecimiento que tenemos se encamina a la idónea atención de nuestros pacientes porque son lo mas importante; es por eso que para nosotros es un compromiso y un deber cuidar la información que nos es suministrada. Acorde y en armonía con la legislación colombiana se da a conocer el articulo 15 de la constitución política:
+  
+  Artículo 15. Todas las personas tienen derecho a su intimidad personal y familiar y a su buen nombre, y el Estado debe respetarlos y h acerlos respetar. De igual modo, tienen derecho a conocer, actualizar y rectificar las informaciones que se hayan recogido sobre ellas en los bancos de datos y en archivos de entidades públicas y privadas. En la recolección, tratamiento y circulación de datos se respetarán la libertad y demás garantías consagradas en la Constitución. La correspondencia y demás formas de comunicación privada son inviolables. Sólo pueden ser interceptados o registrados mediante orden judicial, en los casos y con las formalidades que establezca la ley. Con el fin de prevenir la comisión de actos terroristas, una ley estatutaria reglamentará la forma y condiciones en que las autoridades que ella señale, con fundamento en serios motivos, puedan interceptar o registrar la correspondencia y demás formas de comunicación privada, sin previa orden judicial, con aviso inmediato a la Procuraduría General de la Nación y control judicial posterior dentro de las treinta y seis (36) horas siguientes.
+  
+  Al iniciar cada período de sesiones el Gobierno rendirá informe al Congreso sobre el uso que se haya hecho de esta facultad. Los funcionarios que abusen de las medidas a que se refiere este artículo incurrirán en falta gravísima, sin perjuicio de las demás responsabilidades a que hubiere lugar. Para efectos tributarios judiciales y para los casos de inspección, vigilancia e intervención del Estado, podrá exigirse la presentación de libros de contabilidad y demás documentos privados, en los términos que señale la ley.
+
+  Dentro del marco de leyes colombianas encontramos también la LEY 1266 DE 2008, donde se dictan las disposiciones generales del habeas data y se regula el manejo de la información contenida en la base de datos personales. La LEY 1273 DE 2009 , en esta ley se crea la protección de la información y de los datos, preserva los sistemas que usan las tecnologías de la información y las comunicaciones estableciendo nuevos tipos penales en relación con los delitos informáticos lo que de forma congruente y lógica genera el nacimiento de un nuevo bien jurídico tutelable, tipificando así como una conducta punible la “violación de datos personales” De la misma manera la ley estatutaria 1581 de 2012 posteriormente el Decreto 1377 de 2013 dictan disposiciones generales para la protección de datos personales.
+
+  Centrándonos de forma directa en nuestra actividad económica y en nuestro objeto social traemos al presente la LEY 23 DE 1981 sobre ética medica y su DECRETO REGLAMENTARIO 3380 establecen los principios, valores y reglas para el ejercicio de la medicina, organiza los tribunales de ética y adoptó el régimen disciplinario en sus aspectos sustancial y procedimental para establecer responsabilidad por infracción de las normas. La RESOLUCION 1995 DE 1999 norma emitida por el Ministerio de Salud, hoy Ministerio de Protección Social; es esta resolución quien regula el manejo que debe darse a las historias clínicas en Colombia, establece normas para el manejo, señala las características que debe contener, apertura e identificación de las misma, numeración consecutiva, componentes. Identificación del usuario, registros específicos y anexos.
+
+  Desde la cúspide del régimen jurídico colombiano se estipula que la información propia de la persona hace parte de su intimidad y en conducencia al buen nombre de la misma, por eso Nordvital y todo su equipo de colaboradores tiene pleno conocimiento de esto, por lo que somos responsables de dar el correcto tratamiento a sus datos personales.
+
+  Nordvital IPS siempre está en actitud proactiva para que cualquier retroalimentación aportada por nuestros pacientes la podamos usar como una herramienta para mejorar nuestro servicio y que la atención prestada sea la idónea; por eso contamos con instrumentos que ponemos a disposición en nuestros canales de atención para recibir las PQRSFD ( Peticiones, Quejas, Reclamos , Sugerencias, Felicitaciones y Denuncias), los cuales serán atendidos con la calidad y calidez que nuestros usuarios merecen.
+
+  CANALES DE ATENCIÓN
+  a ) Líneas telefónicas:
+  ✔ Citas particulares 3174309873
+  ✔ Líneas de atención Afiliados Nueva EPS
+  • 300-9135168
+  • 317-5141175
+  ✔ Líneas de atención Afiliados Compensar
+  • 607-5880120
+  • 607-5880130
+  b ) Página Web: Sección virtual:
+  ✔ https://www.nordvitalips.com/contact-us/
+
+  Los anteriores canales harán parte activa de la prestación y atención a nuestros usuarios, Nordvital IPS será diligente y dará tramite pertinente a las pretensiones y/o peticiones que de allí se deslinden, a lo cual daremos tramite en el menor tiempo posible, no obstante a legislación colombiana estipula términos a los cuales nos acogemos, según LEY 1755 DE 2015 y la sentencia C951 DE 2014 , se parte de que todo escrito que genere o exprese pretensión alguna será catalogado como tal y se estipulan términos correspondientes según la motivación.
+
+  Cuando la pretensión sea de forma general el termino para dar respuesta se ajustará a 15 días hábiles, al momento que en estas pretensiones se requiera documentos e información el termino para dar respuesta será por 10 días hábiles y por último si la petición va encaminada a una consulta esta tendrá termino de 30 días hábiles para recibir su respectiva contestación. Dichos términos son susceptibles de prorrogas. Cuando la petición no sea direccionada de forma correcta por el usuario o paciente se correrá traslado ante el competente para que se surta la diligencia correspondiente.
+
+  Es importante mencionar que sin perjuicio de que la respuesta sea negativa o positiva antes las pretensiones del peticionario, se dará por resuelto lo interpuesto por el usuario. Amparados en la sentencia T-059 de 2016:
+
+  “Ahora bien, esta Corporación ha manifestado que una respuesta es suficiente cuando resuelve materialmente la petición y satisface los requerimientos del solicitante, sin perjuicio de que la respuesta sea negativa a las pretensiones del peticionario. La efectividad de la respuesta depende de que se solucione el caso que se plantea. Por último, la congruencia exige que exista coherencia entre lo respondido y lo pedido, de tal manera que la solución verse sobre lo pedido y no sobre un tema semejante, sin que se excluya la posibilidad de suministrar información adicional que se encuentre relacionada con la petición propuesta.”
+
+  También es importante mencionar que al realizar esta respuesta negativa y/o positiva se entenderá que los hechos que motivan la acción ya son superados, al respecto la sentencia T-059 de 2016, la corte explico:
+
+  “El hecho superado tiene ocurrencia cuando lo pretendido a través de la acción de tutela se satisface y desaparece la vulneración o amenaza de los derechos fundamentales invocados por el demandante, de suerte que la decisión que pudiese adoptar el juez respecto del caso específico resultaría a todas luces inocua y, por lo tanto, contraria al objetivo de protección previsto para el amparo constitucional[6]. En este supuesto, no es perentorio incluir en el fallo un análisis sobre la vulneración de los derechos fundamentales cuya protección se demanda, salvo “si considera que la decisión debe incluir observaciones acerca de los hechos del caso estudiado, [ya sea] para llamar la atención sobre la falta de conformidad constitucional de la situación que originó la tutela, o para condenar su ocurrencia y advertir la inconveniencia de su repetición, so pena de las sanciones pertinentes, si así lo considera. De otro lado, lo que sí resulta ineludible en estos casos, es que la providencia judicial incluya la demostración de la reparación del derecho antes del momento del fallo. Esto es, que se demuestre el hecho superado”
+
+  Nordvital IPS pone a disposición sus usuarios estos instrumentos para realizar (Consultas - Agendamiento de citas - gestión de servicios - Educación al usuario).
+
+  La aceptación de estos términos y condiciones constituyen un acuerdo legal y vinculante entre el Usuario de los Canales Virtuales y Nordvital Ips.
+  `
+  },
+
+  en:{
+    title: "Terms and Conditions",
+    text: 
+    `These Terms and Conditions are published to provide relevant information regarding all aspects of the proper and effective use of our digital channels, as well as to ensure the proper processing of data.
+    
+    Nordvital Ips is a company whose corporate purpose is to provide healthcare services. All our efforts and our ongoing growth are focused on delivering the best possible care to our patients, because they are our top priority; that is why we are committed to and consider it our duty to protect the information entrusted to us. In accordance with Colombian law, we hereby refer to Article 15 of the Constitution:
+     
+    Article 15. All persons have the right to personal and family privacy and to a good reputation, and the State must respect these rights and ensure they are respected. Likewise, they have the right to access, update, and correct information collected about them in databases and files maintained by public and private entities. In the collection, processing, and circulation of data, freedom and other guarantees enshrined in the Constitution shall be respected. Correspondence and other forms of private communication are inviolable. They may only be intercepted or recorded by court order, in the cases and under the formalities established by law. In order to prevent the commission of terrorist acts, a statutory law shall regulate the manner and conditions under which the authorities designated therein, based on serious grounds, may intercept or record correspondence and other forms of private communication without a prior court order, with immediate notification to the Attorney General’s Office and subsequent judicial review within the following thirty-six (36) hours.
+    
+    At the beginning of each session, the Government shall submit a report to Congress on the use that has been made of this authority. Officials who abuse the measures referred to in this article shall be guilty of a very serious offense, without prejudice to any other liabilities that may arise. For tax and judicial purposes, and in cases of inspection, supervision, and intervention by the State, the presentation of accounting books and other private documents may be required, under the terms established by law.
+
+    Within the framework of Colombian law, we also find Law 1266 of 2008, which sets forth the general provisions of habeas data and regulates the handling of information contained in personal databases. Law 1273 of 2009 establishes the protection of information and data, safeguards systems that use information and communications technologies, and establishes new criminal offenses related to cybercrimes. This, in a consistent and logical manner, gives rise to a new legally protectable interest, thereby classifying the “violation of personal data” as a punishable offense. Similarly, Statutory Law 1581 of 2012, followed by Decree 1377 of 2013, sets forth general provisions for the protection of personal data.
+
+    Focusing directly on our economic activity and our corporate purpose, we refer to LAW 23 OF 1981 on medical ethics and its REGULATORY DECREE 3380, which establish the principles, values, and rules for the practice of medicine, organize the ethics tribunals, and adopt the disciplinary regime in its substantive and procedural aspects to establish liability for violations of the rules. RESOLUTION 1995 OF 1999, a regulation issued by the Ministry of Health (now the Ministry of Social Protection), governs the handling of medical records in Colombia. It establishes standards for their management, specifies the information they must contain, and outlines procedures for their access, identification, sequential numbering, and components, including user identification, specific records, and attachments.
+
+    Under Colombian law, personal information is considered part of an individual’s privacy and is integral to their good reputation. Nordvital and its entire team of employees are fully aware of this, and we are therefore committed to handling your personal data appropriately.
+
+    Nordvital IPS always takes a proactive approach so that we can use any feedback provided by our patients as a tool to improve our service and ensure that the care we provide is of the highest quality; That is why we have tools available through our customer service channels to receive PQRSFD (Requests, Complaints, Claims, Suggestions, Compliments, and Reports), all of which will be addressed with the quality and warmth our users deserve.
+
+    CUSTOMER SERVICE CHANNELS
+    a ) Phone numbers:
+    ✔ Individual appointments: 3174309873
+    ✔ Nueva EPS Member Service Lines
+    • 300-9135168
+    • 317-5141175
+    ✔ Compensar Member Service Lines
+    • 607-5880120
+    • 607-5880130
+    b ) Website: Online Section:
+    ✔ https://www.nordvitalips.com/contact-us/
+
+    The above channels will play an active role in providing services and support to our users. Nordvital IPS will act diligently and process any claims and/or requests submitted through these channels, addressing them as quickly as possible; however, Colombian law stipulates terms to which we adhere, in accordance with Law 1755 of 2015 and Ruling C951 of 2014. It is understood that any written communication that generates or expresses a claim will be classified as such, and corresponding terms are stipulated based on the grounds for the claim.
+
+    When the request is of a general nature, the response period shall be 15 business days; however, if such requests require documents or information, the response period shall be 10 business days; finally, if the request is for an inquiry, the response period shall be 30 business days. These deadlines are subject to extension. If the request is not properly addressed by the user or patient, it will be forwarded to the appropriate authority so that the necessary action may be taken.
+
+    Es importante mencionar que sin perjuicio de que la respuesta sea negativa o positiva antes las pretensiones del peticionario, se dará por resuelto lo interpuesto por el usuario. Amparados en la sentencia T-059 de 2016:
+
+    “That said, this Court has held that a response is sufficient when it substantively resolves the petition and meets the petitioner’s requirements, even if the response is unfavorable to the petitioner’s claims. The effectiveness of the response depends on whether the case at hand is resolved. Finally, consistency requires that there be coherence between the response and the request, such that the resolution addresses the specific request and not a similar issue, without excluding the possibility of providing additional information related to the proposed request.”
+
+    It is also important to note that by issuing this negative and/or positive response, it will be understood that the facts underlying the action have already been resolved. In this regard, in Judgment T-059 of 2016, the court explained:
+
+    “The matter is deemed resolved when the relief sought through the constitutional protection action is granted and the violation or threat to the fundamental rights invoked by the plaintiff ceases to exist, such that any decision the judge might render regarding the specific case would clearly be moot and, therefore, contrary to the protective purpose of constitutional protection[6]. In this scenario, it is not mandatory to include in the ruling an analysis of the violation of the fundamental rights whose protection is sought, unless “the court considers that the decision should include observations regarding the facts of the case under review, [whether] to draw attention to the lack of constitutional conformity of the situation that gave rise to the tutela, or to condemn its occurrence and warn against its repetition, under penalty of the relevant sanctions, if it so deems. On the other hand, what is indeed unavoidable in these cases is that the judicial ruling include proof of the restoration of the right prior to the time of the ruling. That is, that the fact has been remedied.”
+
+    Nordvital IPS provides its users with these tools to perform the following tasks: (Inquiries - Appointment scheduling - Service management - User education).
+
+    Acceptance of these terms and conditions constitutes a legal and binding agreement between the User of the Virtual Channels and Nordvital Ips.
+  `
+  }
+}
