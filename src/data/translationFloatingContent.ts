5e952ae5b506fe6188a4dd9d0f49e434a74e1777
Jeferson-Ruiz <jefersonruiz578@gmail.com>
2026-04-30 14:48:03 -0500

    feat(translation): crear componente TranslationFloatingContent para modales

---
+++ src/data/translationFloatingContent.ts
@@ -0,0 +1,64 @@
+export const translationFloatingContent={
+
+  es: {
+    modals: {
+      ethicsCommittee:"Comité Ético"
+    },
+
+    ethicsCommittee: {
+      title: "Comité de Ética en la Investigación CAIMED",
+      info: [
+        "Dirección: Carrera 42A N°17-50, Bogotá D.C",
+        "Teléfono: 350 318 9822",
+        "Email: comitedeetica@caimed.com",
+      ],
+      titleWebside: "Conoce más de CAIMED"
+    },
+
+    avanzar: {
+      title: "Asociación para el avance de la investigación clínica en Colombia.",
+      text: "la Asociación para el Avance de la Investigación Clínica (IC) en Colombia, es una entidad científica sin ánimo de lucro, que desde 1999, a través de la educación no continuada y otras acciones, incentiva, favorece, propicia, asesora, facilita y contribuye al desarrollo de la Investigación Clínica (IC) en el país.",
+      titleWebside: "Conoce más de AVANZAR"
+    },
+
+    metricsmed: {
+      info: [
+        "Radicación y consulta durante las 24 horas del día",
+        "Que  el centro de investigación  y el comité de ética tengan un funcionamiento eficaz y activo para revisión, y seguimiento de proyectos.",
+        "Reducción de los tiempos de respuesta entre la radicación de solicitudes y la emisión de conceptos por parte del comité, promoviendo una interacción más eficiente entre los centros de investigación y el comité de ética"
+      ],
+      titleWebside: "Conoce más de METRICSMED"
+    }
+  },
+
+  en: {
+    modals: {
+      ethicsCommittee:"Ethics Committee"
+    },
+    
+    ethicsCommittee: {
+      title: "CAIMED Research Ethics Committee",
+      info: [
+        "Address: Carrera 42A N°17-50, Bogotá D.C",
+        "Phone: 350 318 9822",
+        "Email: comitedeetica@caimed.com",
+      ],
+      titleWebside: "Learn more about CAIMED"
+    },
+
+    avanzar: {
+      title: "Association for the Advancement of Clinical Research in Colombia.",
+      text: "The Association for the Advancement of Clinical Research (IC) in Colombia is a nonprofit scientific organization that, since 1999, has been promoting, supporting, fostering, advising, facilitating, and contributing to the development of clinical research (IC) in the country through continuing education and other initiatives.",
+      titleWebside: "Learn more about AVANZAR"
+    },
+
+    metricsmed: {
+      info: [
+        "24-hour on-call and consultation services",
+        "The research center and the ethics committee should operate effectively and proactively to review and monitor projects.",
+        "Reducing the turnaround time between the submission of applications and the committee’s issuance of opinions, thereby promoting more efficient interaction between research centers and the ethics committee"
+      ],
+      titleWebside: "Learn more about METRICSMED"
+    }
+  }
+}
