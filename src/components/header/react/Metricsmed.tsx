4b89101c17105ee8a13bb65d31d724bbf45549ca
Jeferson-Ruiz <jefersonruiz578@gmail.com>
2026-04-30 14:52:53 -0500

    refactor(Metricsmed): integrar useTranslation con translationFloatingContent

--- src/components/header/react/Metricsmed.tsx
+++ src/components/header/react/Metricsmed.tsx
@@ -1,26 +1,22 @@
+import { useTranslation } from "@/hooks/useTranslation";
 import { FloatingWindowContent } from "../../ui/FloatingWindowContent"
 import metricsmed from '@logo/metricsmed.webp'
+import { translationFloatingContent } from "@/data/translationFloatingContent";
 
-const TITLE = "METRICSMED "
-
-const INFO = [
-  "Radicación y consulta durante las 24 horas del día",
-  "Que  el centro de investigación  y el comité de ética tengan un funcionamiento eficaz y activo para la revisión, y seguimiento de proyectos.",
-  "Reducción de los tiempos de respuesta entre la radicación de solicitudes y la emisión de conceptos por parte del comité, promoviendo una interacción más eficiente entre los centros de investigación y el comité de ética"
-]
-
-const TITLE_WEBSIDE = "Conoce más de METRICSMED";
+const TITLE = "METRICSMED"
 
 const WEBSITE = "https://metricsmed.com/";
 
 export function Metricsmed(){
+  const { t } = useTranslation(translationFloatingContent);
+  
   return(
     <FloatingWindowContent
       image={metricsmed.src}
       title={TITLE}
-      information={INFO}
-      titleWebsite={TITLE_WEBSIDE}
+      information={t("metricsmed.info")}
+      titleWebsite={t("metricsmed.titleWebside")}
       website={WEBSITE}
-      />
+    />
   )
 }
