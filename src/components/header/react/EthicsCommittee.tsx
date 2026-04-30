f7fd5830a9a229726c079279d8c4eb891a1b7d02
Jeferson-Ruiz <jefersonruiz578@gmail.com>
2026-04-30 14:52:20 -0500

    refactor(EthicsCommittee): integrar useTranslation con translationFloatingContent

--- src/components/header/react/EthicsCommittee.tsx
+++ src/components/header/react/EthicsCommittee.tsx
@@ -1,8 +1,9 @@
+import { useTranslation } from "@/hooks/useTranslation";
 import { FloatingWindowContent } from "../../ui/FloatingWindowContent";
 import caimedlogo from '@logo/caimedlogo.webp'
+import { translationFloatingContent } from "@/data/translationFloatingContent";
 
-
-const TILTE = " Comité de Ética en la Investigación CAIMED";
+const TILTE = "Comité de Ética en la Investigación CAIMED";
 
 const information = [
   "Dirección: Carrera 42A N°17-50, Bogotá D.C",
@@ -10,17 +11,18 @@ const information = [
   "Email: comitedeetica@caimed.com",
 ]
 
-const titleWebside = "Conoce más de CAIMED";
 const website = "https://caimed.com/comite-de-etica/";
 
-
 export function EthicsCommittee() {
+
+  const { t } = useTranslation(translationFloatingContent);
+
   return (
     <FloatingWindowContent
       image={caimedlogo.src}
-      title= {TILTE}
-      information = {information}
-      titleWebsite={titleWebside}
+      title= {t("ethicsCommittee.title")}
+      information = {t("ethicsCommittee.info")}
+      titleWebsite={t("ethicsCommittee.titleWebside")}
       website={website}
     />
   );
