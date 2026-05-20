2e4ff02bd2a733ba841ac50a4f98f3bb3138f5fa
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-05-20 09:15:03 -0500

    feat(translation): agregar contenido a la seccion de featuredCard, mejorar indentado

--- src/data/translationBlog.ts
+++ src/data/translationBlog.ts
@@ -4,6 +4,7 @@ export const translations = {
     lineText: "Publicaciones recientes",
 
     featuredCard: {
+      featured: "DESTACADO",
       date: "26 Abril 2026",
       span: "Curso Internacional",
       title: "Participación destacada en el Curso Internacional ENDIMET",
@@ -11,8 +12,10 @@ export const translations = {
       text: `
         La IPS y el Centro de investigación, participaron en el Curso Internacional ENDIMET de la Asociación Colombiana de Endocrinología, Diabetes y Metabolismo, un importante escenario académico que reunió investigaciones relevantes en el área.
         En esta edición se postularon un total de 226 E-poster, entre los cuales nuestra institución presentó los trabajos:
-        - Tamizaje racional de la vitamina D
-        - ¿A quién tamizar para hipotiroidismo clínico? Evidencia epidemiológica desde atención primaria
+
+        - Tamizaje racional de la vitamina D.
+        - ¿A quién tamizar para hipotiroidismo clínico? Evidencia epidemiológica desde atención primaria.
+
         Ambos estudios fueron aceptados para presentación, destacándose por su rigor metodológico y pertinencia clínica.
         De manera especial, el E-poster Tamizaje racional de la vitamina D fue seleccionado para defensa oral, logrando posicionarse dentro de los 29 mejores trabajos del evento.
         Este reconocimiento resalta el compromiso institucional con la investigación científica y la generación de evidencia para la práctica clínica en atención primaria.`
@@ -20,7 +23,8 @@ export const translations = {
     endimet: {
       span: "Curso",
       title: "Curso Internacional ENDIMET",
-      text: "La La IPS y el Centro de investigación hicieron parte del Curso Internacional ENDIMET de la Asociación Colombiana de Endocrinología, Diabetes y Metabolismo, un reconocido encuentro académico que reunió importantes investigaciones del sector."
+      text: "La La IPS y el Centro de investigación hicieron parte del Curso Internacional ENDIMET de la Asociación Colombiana de Endocrinología, Diabetes y Metabolismo, un reconocido encuentro académico que reunió importantes investigaciones del sector.",
+      date: "26 Abril 2026",
     }
   },
 
@@ -29,6 +33,7 @@ export const translations = {
     lineText: "Recent Publications",
 
     featuredCard: {
+      featured: "FEATURED",
       date: "April 26, 2026",
       span: "International Course",
       title: "Notable Participation in the ENDIMET International Course",
@@ -36,16 +41,20 @@ export const translations = {
       text: `
         IPS and the Research Center participated in the ENDIMET International Course organized by the Colombian Association of Endocrinology, Diabetes, and Metabolism, a major academic event that brought together leading research in the field.
         A total of 226 e-posters were submitted for this edition, among which our institution presented the following studies:
+
           - Rational screening for vitamin D
-          - Who should be screened for clinical hypothyroidism? Epidemiological evidence from primary care
+          - Who should be screened for clinical hypothyroidism? Epidemiological evidence from primary care.
+          
         Both studies were accepted for presentation, standing out for their methodological rigor and clinical relevance.
         In particular, the e-poster titled “Rational Vitamin D Screening” was selected for an oral presentation, ranking among the top 29 presentations at the event.
         This recognition highlights the institution's commitment to scientific research and the generation of evidence for clinical practice in primary care.`
     },
     endimet: {
+      featured: "FEATURED",
       span: "Course",
       title: "ENDIMET International Course",
-      text: "IPS and the Research Center participated in the ENDIMET International Course organized by the Colombian Association of Endocrinology, Diabetes, and Metabolism, a renowned academic conference that brought together leading research in the field."
+      text: "IPS and the Research Center participated in the ENDIMET International Course organized by the Colombian Association of Endocrinology, Diabetes, and Metabolism, a renowned academic conference that brought together leading research in the field.",
+      date: "April 26, 2026",
     }
   }
 } 
