7611004bd4cabdd2264d3bf7fc900c4b730f7abb
Jeferson-Ruiz <jefersonruiz587@gmail.com>
2026-05-21 09:25:01 -0500

    fix(contact): actualizar imagen de Caimed e información de dirección y teléfono

--- src/components/header/react/EthicsCommittee.tsx
+++ src/components/header/react/EthicsCommittee.tsx
@@ -1,12 +1,12 @@
 import { useTranslation } from "@/hooks/useTranslation";
 import { FloatingWindowContent } from "../../ui/FloatingWindowContent";
-import caimedlogo from '@logo/caimedlogo.webp'
+import caimedlogoactualizado from '@logo/caimedlogoactualizado.webp'
 import { translationFloatingContent } from "@/data/translationFloatingContent";
 
 const WEBSITE = "https://caimed.com/comite-de-etica/";
 
-const ADDRESS = "Carrera 42A N°17-50, Bogotá D.C";
-const PHONE = "350 318 9822";
+const ADDRESS = "Carrera 4 Este N.° 24-65 Chía, Cundinamarca.";
+const PHONE = "(+57) 316 741 6771";
 const EMAIL = "Email: comitedeetica@caimed.com";
 const TITLE = "CAIMED";
 
@@ -23,7 +23,7 @@ export function EthicsCommittee() {
   
   return (
     <FloatingWindowContent
-      image={caimedlogo.src}
+      image={caimedlogoactualizado.src}
       title= {t("ethicsCommittee.title")}
       information = {info}
       titleWebsite={TITLE_WEBSIDE}
